9ba0ae6e34137f5af7ffe1a67eb3c0377864b77d
ruonanqwe <123006077+ruonanqwe@users.noreply.github.com>
2025-02-17 15:05:08 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,50 +1,11 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
     domains: ['netlify.app']
   },
-  experimental: {
-    webpackBuildWorker: true,
-  },
-  output: 'export',
-  distDir: 'dist',
-  trailingSlash: true,
-}
-
-mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
+  distDir: '.next'
 }
 
 export default nextConfig
